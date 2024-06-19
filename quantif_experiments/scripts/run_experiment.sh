f72b11a67aa5d629a13208c1950aad5b919a2678
roi <rodrigo.perez@urjc.es>
2024-06-19 14:51:00 +0200

    experiments wip

--- quantif_experiments/scripts/run_experiment.sh
+++ quantif_experiments/scripts/run_experiment.sh
@@ -2,18 +2,29 @@
 
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
+rmw=$1
+arch=$2
+
 # Define the experiment parameters
-times=(1 2)
+times=(1 2) # senconds
 use_ipc_values=(0 1)
 load_values=("low" "medium" "high")
 experiment_path=$THIS_DIR/../results
-arch="simple"
-rmw="fast"
 
+# Validate RMW implementation
 if [[ "$rmw" == "fast" ]]; then
   RMW=rmw_fastrtps_cpp
 elif [[ "$rmw" == "cyclone" ]]; then
   RMW=rmw_cyclonedds_cpp
+else
+  echo "Unknown RMW implementation: $rmw"
+  return
+fi
+
+# Validate architecture
+if [[ "$arch" != "simple" && "$arch" != "medium" && "$arch" != "complex" ]]; then
+  echo "Invalid architecture: $arch. Valid options are 'simple', 'medium', or 'complex'."
+  return
 fi
 
 echo "Using RMW_IMPLEMENTATION=${RMW}"
@@ -28,7 +39,7 @@ fi
 # Invoke RMW-specific script forwarding all command-line arguments to it
 source $RMW_SPECIFIC_SETUP_SCRIPT "$@"
 
-# Function to run stress command
+# Function to run high stress command
 run_high_stress() {
   local duration=$1
   echo "Running stress command in background for $duration seconds"
@@ -36,21 +47,28 @@ run_high_stress() {
   stress_pid=$!
 }
 
+exp=0
 # Loop over the times, use_ipc values, and load values
 for t in "${times[@]}"; do
   for use_ipc in "${use_ipc_values[@]}"; do
     for load in "${load_values[@]}"; do
-      echo "Running experiment with -t $t, --use_ipc $use_ipc, and --load $load"
-
-    # Run stress command
+      exp=$((exp+1))
+      echo "-------------------------------------------------"
+      echo "Running experiment $exp, with -t $t, --use_ipc $use_ipc, and --load $load"
+      # Run stress command
       if [ "$load" == "high" ]; then
         run_high_stress $t
       fi
 
       ros2 run quantif_experiments quantif --use_ipc $use_ipc -t $t --experiment_path "$experiment_path" --arch $arch --rmw $rmw --load $load --verbose 0
+      
       echo "Experiment with -t $t, --use_ipc $use_ipc, and --load $load completed"
+      echo "-------------------------------------------------"
     done
   done
 done
 
-echo "All experiments completed"
+echo ""
+echo "ALL $exp EXPERIMENTS COMPLETED"
+echo ""
+echo "-------------------------------------------------"
