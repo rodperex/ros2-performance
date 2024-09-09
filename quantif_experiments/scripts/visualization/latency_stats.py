69907d52f2d0e9528379e7dd0a30225811b809b5
Fran <butakus@gmail.com>
2024-09-09 16:29:49 +0200

    Add latency/load visuzlization

--- quantif_experiments/scripts/visualization/latency_stats.py
+++ quantif_experiments/scripts/visualization/latency_stats.py
@@ -2,8 +2,45 @@
 """ Script to compute and show different latency metrics """
 
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 import parser  # Local quantif parser file
 
+def plot_latency_over_size(lat_data, arch='simple'):
+  """ Plot latency (avg and std) over message size.
+  """
+  # Filter columns
+  lat_data = lat_data[['Size', 'Mean', 'Std', 'Pub/Sub Type', 'Architecture', 'CPU Stress', 'RMW', 'IPC']]
+  # Use only the experiments without CPU stress
+  lat_data = lat_data[lat_data['CPU Stress'] == 'low']
+  lat_data = lat_data[lat_data['Architecture'] == arch]
+  # Use only the publishers latency for first subplot and subscription for second
+  lat_data_pubs = lat_data[lat_data['Pub/Sub Type'] == 'Publisher']
+  lat_data_subs = lat_data[lat_data['Pub/Sub Type'] == 'Subscription']
+  print(F"Publishers Latency:\n{lat_data_pubs}")
+  print(F"Subscriptions Latency:\n{lat_data_subs}")
+  g = sns.catplot(kind='bar', data=lat_data, x='Size', y='Mean', hue='IPC', col='RMW', row='Pub/Sub Type', errorbar=None)
+  g.set_xlabels('Message Size (Bytes)', fontsize=15)
+  g.set_ylabels('Average Latency (µs)', fontsize=15)
+  plt.show()
+
+def plot_latency_over_load(lat_data, arch='simple'):
+  """ Plot latency (avg and std) over CPU load.
+  """
+  # Filter columns
+  lat_data = lat_data[['Size', 'Mean', 'Std', 'Pub/Sub Type', 'Architecture', 'CPU Stress', 'RMW', 'IPC']]
+  # Use only the experiments without CPU stress
+  lat_data = lat_data[lat_data['IPC'] == 1]
+  lat_data = lat_data[lat_data['Architecture'] == arch]
+  # Use only the publishers latency for first subplot and subscription for second
+  lat_data_pubs = lat_data[lat_data['Pub/Sub Type'] == 'Publisher']
+  lat_data_subs = lat_data[lat_data['Pub/Sub Type'] == 'Subscription']
+  print(F"Publishers Latency:\n{lat_data_pubs}")
+  print(F"Subscriptions Latency:\n{lat_data_subs}")
+  g = sns.catplot(kind='bar', data=lat_data, x='Size', y='Mean', hue='CPU Stress', col='RMW', row='Pub/Sub Type', errorbar=None)
+  g.set_xlabels('Message Size (Bytes)', fontsize=15)
+  g.set_ylabels('Average Latency (µs)', fontsize=15)
+  plt.show()
 
 
 def main():
@@ -21,8 +58,12 @@ def main():
     'IPC'
   ]
   print(F"Latency data filtered:\n{latency_data[print_cols]}")
-  latency_data.to_csv("latency_data.csv")
 
+  # Save to CSV for outside analysis
+  # latency_data.to_csv("latency_data.csv")
+
+  plot_latency_over_size(latency_data, arch='complex')
+  plot_latency_over_load(latency_data, arch='complex')
 
 if __name__ == '__main__':
     main()
